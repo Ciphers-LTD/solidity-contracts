65743a53830d6606a0d80d84937bf3b4001db5c0
ubaid-720 <79498396+ubaid-720@users.noreply.github.com>
2021-07-13 17:21:46 +0500

    Add files via upload

---
+++ Auction/auctionfactory.sol
@@ -0,0 +1,24 @@
+  
+pragma solidity ^0.4.8;
+
+import { Auction } from './auction.sol';
+
+contract AuctionFactory {
+    address[] public auctions;
+
+    event AuctionCreated(address auctionContract, address owner, uint numAuctions, address[] allAuctions);
+
+    function AuctionFactory() {
+    }
+
+    function createAuction(uint bidIncrement) {
+        Auction newAuction = new Auction(msg.sender, bidIncrement);
+        auctions.push(newAuction);
+
+        AuctionCreated(newAuction, msg.sender, auctions.length, auctions);
+    }
+
+    function allAuctions() constant returns (address[]) {
+        return auctions;
+    }
+}
