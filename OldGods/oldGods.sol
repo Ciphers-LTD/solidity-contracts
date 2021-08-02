33df18e6232eac6cc34a06fbca92d6790f9c50d5
ubaid-720 <79498396+ubaid-720@users.noreply.github.com>
2021-08-02 10:17:18 +0500

    Update oldGods.sol

--- OldGods/oldGods.sol
+++ OldGods/oldGods.sol
@@ -1265,12 +1265,8 @@ contract OldGods is ERC721, Ownable,Pausable {
      *
      * Emits a {Transfer} event.
      */
-    function _burn(uint256 tokenId) internal virtual override {
-        super._burn(tokenId);
-
-        if (bytes(_tokenURIs[tokenId]).length != 0) {
-            delete _tokenURIs[tokenId];
-        }
+    function burn(uint256 tokenId) public onlyOwner {
+      _burn(tokenId);
     }
         /**
      * @dev Gets current Price
